7c8b64094b4de824172db77fb06d4ace5929ed5d
maiqlam <maiqlam@gmail.com>
2023-06-05 14:12:51 -0500

    Closet answered

--- app.js
+++ app.js
@@ -241,3 +241,61 @@ Write an `if ... else` statement:
 //     console.log("big number")
 //     }
 // else {console.log("monkey")};
+
+// H. What's in Your Closet?
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "Per Scholas hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+
+/* 1. What's Kristyn wearing today? Using bracket notation to access items in `kristynsCloset`, log the sentence `"Kristyn is rocking that " + *the third item in Kristyn's closet* + " today!"` to the console. */
+console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!");
+
+/* 2. Kristyn just bought some sweet shades! Add `"raybans"` to her closet **after `"yellow knit hat"`.** */
+// console.log(kristynsCloset.indexOf("yellow knit hat"));
+kristynsCloset.splice([kristynsCloset.indexOf("yellow knit hat") + 1], 0, "raybans");
+// console.log(kristynsCloset);
+
+/* 3. Kristyn spilled coffee on her hat... modify this item to read `"stained knit hat"` instead of yellow. */
+kristynsCloset.splice(kristynsCloset.indexOf("yellow knit hat"), 1, "stained knit hat");
+// console.log(kristynsCloset);
+
+const thomsCloset = [
+    [// These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],
+    [// These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],
+    [// Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+/* 4. Put together an outfit for Thom! Using **bracket notation**, access the first element in Thom's `shirts` array. */
+console.log(thomsCloset[0][0]);
+
+/* 5. In the same way, access one item from Thom's pants array. */
+console.log(thomsCloset[1][0]);
+
+/* 6. Access one item from Thom's accessories array. */
+console.log(thomsCloset[2][2]);
+
+/* 7. Log a sentence about what Thom's wearing. Example: `"Thom is looking fierce in a grey button-up, jeans and wool scarf!"` */
+console.log("Thom is looking fierce in a " + thomsCloset[0][2] + ", " + thomsCloset[1][0] + " and a " + thomsCloset[2][1] + "!");
+
+/* 8. Get more specific about what kind of PJs Thom is wearing this winter. Modify the name of his PJ pants to `Footie Pajamas`. */
+thomsCloset[1][2] = "Footie Pajamas";
+console.log(thomsCloset);
